e4126ef49f547c00e201cca4bdcd78243e4bae71
Sebastian <sebastian.callh@gmail.com>
2018-09-12 12:02:23 +0200

    Remove MonadError constraint on scraper functions

--- src/CourseScalpel.hs
+++ src/CourseScalpel.hs
@@ -9,29 +9,34 @@ module CourseScalpel
   , mkConfig
   ) where
 
-import           Data.Text                         (Text)
 import           Data.Semigroup                    ((<>))
+import           Data.Text                         (Text)
 
-import qualified CourseScalpel.CoursePage          as CoursePage
 import           CourseScalpel.App                 (App, Config (..), runApp)
+import           CourseScalpel.CoursePage          as X (CoursePage,
+                                                         MonadCoursePage)
+import qualified CourseScalpel.CoursePage          as CoursePage
+import           CourseScalpel.CoursePage.Occasion as X (Block (..),
+                                                         Importance (..),
+                                                         Occasion (..),
+                                                         Period (..),
+                                                         Semester (..))
+import           CourseScalpel.Error               (Error (..))
 import           CourseScalpel.Program             (Program)
+import           CourseScalpel.Program             as X (engD, engDPU, engED,
+                                                         engEMM, engI, engIInt,
+                                                         engIT, engKB, engKTS,
+                                                         engM, engMT, engMed,
+                                                         engTB, engU, engY,
+                                                         engYInt,
+                                                         supportedPrograms)
 import qualified CourseScalpel.Program             as Program
-import           CourseScalpel.Error               (Error (..), MonadError)
+import           CourseScalpel.ProgramPage         as X (MonadProgramPage)
 import qualified CourseScalpel.ProgramPage         as ProgramPage
 import           CourseScalpel.Web                 (Url (..))
-import           CourseScalpel.CoursePage          as X (CoursePage, MonadCoursePage)
-import           CourseScalpel.ProgramPage         as X (MonadProgramPage)
-import           CourseScalpel.CoursePage.Occasion as X (Occasion (..), Semester (..)
-                                                        , Period (..), Block (..)
-                                                        , Importance (..))
-import           CourseScalpel.Program             as X (engD, engU, engI, engIInt
-                                                        , engIT, engY, engYInt, engMed
-                                                        , engMT, engED, engKTS, engM
-                                                        , engEMM, engTB, engDPU, engKB
-                                                        , supportedPrograms)
 
-type ProgramPageScrapeResult = ProgramPage.ScrapeResult 
-type CoursePageScrapeResult = CoursePage.ScrapeResult 
+type ProgramPageScrapeResult = ProgramPage.ScrapeResult
+type CoursePageScrapeResult = CoursePage.ScrapeResult
 type CourseScalpelRunner a = App a -> IO (Either Error a)
 
 runCourseScalpel :: Config -> CourseScalpelRunner a
@@ -40,23 +45,13 @@ runCourseScalpel = runApp
 mkConfig :: FilePath -> Config
 mkConfig = Config
 
-scrapeProgramPage
-  :: forall m
-  . (MonadProgramPage m,
-     MonadError m)
-  => Program
-  -> m ProgramPage.ScrapeResult
+scrapeProgramPage :: MonadProgramPage m => Program -> m ProgramPage.ScrapeResult
 scrapeProgramPage program = ProgramPage.scrapeProgramPage url
   where
     url = Url $ "https://liu.se/studieinfo/program/"
           <> Program.slugToText (Program.slug program)
 
-scrapeCoursePage
-  :: forall m
-  . (MonadCoursePage m,
-     MonadError m)
-  => Text
-  -> m CoursePage.ScrapeResult
+scrapeCoursePage :: MonadCoursePage m => Text -> m CoursePage.ScrapeResult
 scrapeCoursePage code = CoursePage.scrapeCoursePage url
   where
     url = Url $"https://liu.se/studieinfo/kurs/" <> code
